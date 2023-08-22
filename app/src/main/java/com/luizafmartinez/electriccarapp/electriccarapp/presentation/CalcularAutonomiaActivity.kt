86a54d34ed883f107907c02087db9ac7c87b6e90
alxmart <alxmartinez@gmail.com>
2023-08-22 16:11:57 -0300

    Adding calculation.

--- app/src/main/java/com/luizafmartinez/electriccarapp/electriccarapp/presentation/CalcularAutonomiaActivity.kt
+++ app/src/main/java/com/luizafmartinez/electriccarapp/electriccarapp/presentation/CalcularAutonomiaActivity.kt
@@ -1,13 +1,51 @@
 package com.luizafmartinez.electriccarapp.electriccarapp.presentation
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.luizafmartinez.electriccarapp.R
 
 class CalcularAutonomiaActivity : AppCompatActivity() {
 
+    lateinit var preco: EditText
+    lateinit var kmPercorrido: EditText
+    lateinit var resultado: TextView
+    lateinit var btnCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calcular_autonomia)
+        setupView()
+        setupListener()
     }
+
+    fun setupView() {
+        preco = findViewById<EditText>(R.id.et_preco_kwh)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        resultado = findViewById(R.id.tv_resultado)
+        btnCalcular = findViewById(R.id.btn_calcular)
+    }
+
+    fun setupListener() {
+        btnCalcular.setOnClickListener {
+            calcular()
+        }
+    }
+
+    fun calcular() {
+        val preco = preco.text.toString().toFloat()
+        val km = kmPercorrido.text.toString().toFloat()
+        val result = preco / km
+        resultado.text = result.toString()
+    }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/luizafmartinez/electriccarapp/electriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/electriccarapp/electriccarapp/presentation/MainActivity.kt
@@ -10,30 +10,21 @@ import com.luizafmartinez.electriccarapp.R
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var preco: EditText
-    lateinit var kmPercorrido: EditText
-    lateinit var btnCalcular: Button
-    lateinit var resultado: TextView
+   lateinit var btnCalcular: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setupView()
         setupListener()
     }
 
     fun setupView() {
-//        preco = findViewById<EditText>(R.id.et_preco_kwh)
-//        kmPercorrido = findViewById(R.id.et_km_percorrido)
-//        resultado = findViewById(R.id.tv_resultado)
-//        btnCalcular = findViewById<Button>(R.id.btn_calcular)
           btnCalcular = findViewById(R.id.btn_calcular)
     }
 
     fun setupListener() {
         btnCalcular.setOnClickListener {
-            //calcular()
             val intent = Intent(
                 this,
                 CalcularAutonomiaActivity::class.java
@@ -42,10 +33,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun calcular() {
-        val preco = preco.text.toString().toFloat()
-        val km = kmPercorrido.text.toString().toFloat()
-        val result = preco / km
-        resultado.text = result.toString()
-    }
 }
